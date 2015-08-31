ec65b5ed5176a9bfea57bde3f82694ca8cc9ae06
scott7950 <scott0926@outlook.com>
2015-08-31 07:50:33 -0700

    add example of inverse a string
    add example of inverse a string

---
+++ c/inverse_char_string.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+void inverse(char* str) {
+    int size = 0;
+    int i;
+    char tmp;
+
+    while(str[size] != '\0') {
+        size++;
+    }
+
+    for(i=0; i<size/2; i++) {
+        tmp = *(str + i);
+        *(str + i) = *(str + size - 1 - i);
+        *(str + size - 1 - i) = tmp;
+    }
+}
+
+int main(int argc, char* argv[]) {
+    //char* s = "Hello world";
+    //puts the string literal "Hello world" in read-only memory and creates a pointer s to that, 
+    //making any writing operation on s illegal (e.g., s[0] = 'A'), 
+    //while char s[] = "Hello world";
+    //puts the string literal "Hello world" in read-only parts of memory, 
+    //creates an array s by allocating memory on the stack and copies the string literal to the newly allocated memory, 
+    //making any writing operation on s legal.
+    char str1[] = "Hello World";
+    char str2[] = "HelloWorld";
+    char str3[] = "";
+    char str4[] = "H";
+    char str5[] = "He";
+
+    inverse(str1);
+    printf("%s\n", str1);
+
+    inverse(str2);
+    printf("%s\n", str2);
+
+    inverse(str3);
+    printf("%s\n", str3);
+
+    inverse(str4);
+    printf("%s\n", str4);
+
+    inverse(str5);
+    printf("%s\n", str5);
+
+    return 0;
+}
+
